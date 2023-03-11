1f6bd342ddb72ab99fffe813d9910b4f31e90e8c
= <=>
2023-03-11 21:30:02 +0800

    feat: index frame finish

---
+++ src/components/CopyrightFooter.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="footer">
+    <ul class="copyright">
+      <li>&copy; 2020-2023 astercasc.com 版权所有 互联网ICP备案：
+        <a target="_blank" href="https://beian.miit.gov.cn/">浙ICP备2022023127号</a>
+      </li>
+      <li>新浪微博: <a target="_blank" href="https://weibo.com/u/5177219464">AsterCass</a></li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "CopyrightFooter"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/cask.sass";
+
+.footer {
+  background: $cask_dune;
+  padding: 0.5em 0 1.5em 0;
+
+  a {
+    color: inherit;
+  }
+
+  .copyright {
+    color: #bbb;
+    font-size: 0.9em;
+    margin: 2em 0 0 0;
+    padding: 0;
+    text-align: center;
+  }
+
+  .copyright li {
+    border-left: solid 1px rgba(144, 144, 144, 0.25);
+    display: inline-block;
+    list-style: none;
+    margin-left: 1.5em;
+    padding-left: 1.5em;
+  }
+
+  .copyright li:first-child {
+    border-left: 0;
+    margin-left: 0;
+    padding-left: 0;
+  }
+
+  .icons a {
+    color: #bbb;
+  }
+}
+
+</style>
--- src/pages/AdminIndex.vue
+++ src/pages/AdminIndex.vue
@@ -1,14 +1,6 @@
 <template>
-
-  <!--  <div class="roller-guide">-->
-  <!--    <img-->
-  <!--        v-scroll-fire="bounceImage"-->
-  <!--        src="https://cdn.quasar.dev/logo-v2/svg/logo.svg"-->
-  <!--        style="height: 100px; width: 100px;"-->
-  <!--        alt="">-->
-  <!--  </div>-->
-
   <q-layout view="lHh lpr lFf" container:false>
+
     <q-header :modelValue="headerVisible" class="index-header">
       <q-toolbar>
         <q-toolbar-title>Cask Admin</q-toolbar-title>
@@ -19,7 +11,7 @@
     <div>
       <q-img class="index-img"
              :no-native-menu="false"
-             :src="require('../assets/img/MengPo.jpg')"
+             :src="require('../assets/img/backgroud.png')"
              alt=""
              :ratio="16/9"
              :fit="'cover'">
@@ -32,45 +24,42 @@
           <p v-for="n in 100" :key="n">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci,
             dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus
-            commodi perferendis voluptate?
+            commodi perferendis voluptate? 字体观察
           </p>
 
         </q-page>
       </q-page-container>
     </div>
 
-    <q-page-sticky position="bottom" :offset="[0, 150]">
+    <CopyrightFooter/>
+
+
+    <!--        https://animate.style/-->
+    <q-page-sticky position="bottom" :offset="[0, 100]">
       <div :hidden="rollerGuideHidden">
         <q-img
-            :disabled="true"
-            v-scroll-fire="bounceImage"
-            src="https://cdn.quasar.dev/logo-v2/svg/logo.svg"
-            style="height: 100px; width: 100px;"
+            class="roller-guide"
+            :src="require('../assets/ico/downward_line.png')"
+            style="height: 150px; width: 150px;"
             alt=""/>
       </div>
     </q-page-sticky>
-
   </q-layout>
-
-
-
-  <!--  <q-btn push-->
-  <!--         color="primary"-->
-  <!--         label="Push"-->
-  <!--         @click="toDoc">-->
-  <!--    this is a push-->
-  <!--  </q-btn>-->
-
-
 </template>
 
 <script>
 import {ref} from 'vue'
+import CopyrightFooter from '@/components/CopyrightFooter.vue'
+import 'animate.css';
+
 
 export default {
   name: "AdminIndex",
+  components: {CopyrightFooter},
   setup() {
-    // https://quasar.dev/vue-directives/intersection
+
+// https://quasar.dev/vue-directives/intersection
+// https://quasar.dev/vue-directives/scroll-fire
     let headerVisible = ref(false)
     let rollerGuideHidden = ref(false)
 
@@ -79,44 +68,37 @@ export default {
       rollerGuideHidden.value = entry.isIntersecting
     }
 
+    // //页面滚动事件捕捉
+    // function doScroll({target: {documentElement: {scrollTop, clientHeight, scrollHeight}}}) {
+    //   if(Math.abs(scrollTop + clientHeight - scrollHeight) < 1) {
+    //     console.log("bottom")
+    //   } else {
+    //     console.log("not bottom")
+    //   }
+    // }
+    // onMounted(() => {
+    //   window.addEventListener('scroll', doScroll)
+    // })
+    // onUnmounted(() => {
+    //   window.removeEventListener('scroll', doScroll)
+    // })
 
-    //return
+
     return {
       headerVisible,
-      onIntersection,
       rollerGuideHidden,
-
-      // https://quasar.dev/vue-directives/scroll-fire
-      bounceImage(el) {
-        // in this example, when the `<div>` comes into view,
-        // we bounce it for 2 seconds
-
-        el.classList.add('animate-bounce')
-
-        setTimeout(() => {
-          // we make sure the node is still in DOM
-          // (user hasn't navigated away from the Vue component
-          // rendering our `<div>`)
-          // so we don't generate an error
-          if (document.body.contains(el)) {
-            // then remove the helper class to
-            // stop bouncing
-            el.classList.remove('animate-bounce')
-          }
-        }, 20000)
-      }
-
-
+      onIntersection,
     }
-  }
 
+  }
 }
 </script>
 
 <style lang="sass" scoped>
+@import "@/styles/cask.sass"
 
 .roller-guide
-  text-align: center
+  animation: fadeOutDown 1.7s infinite
 
 .index-img
   z-index: -1
@@ -125,14 +107,12 @@ export default {
 //离谱的是如果希望达到不透明的效果需要 background-color: white 中来调整a的比例
 .index-main
   text-align: center
-  margin-top: -5rem
-  margin-right: 5rem
-  margin-left: 5rem
+  margin: -5rem 5rem 5rem 5rem
   //background-color: white
   //opacity: 0.8
   background-color: rgba(255, 255, 255, 0.8)
   box-shadow: inset 0 0 1px 1px rgba(254, 254, 254, 0.9), 0 20px 27px 0 rgba(0, 0, 0, 0.05)
-  //backdrop-filter: saturate(200%) blur(30px)
+  backdrop-filter: saturate(200%) blur(30px)
   border-radius: 15px
 
 .index-header
@@ -144,19 +124,8 @@ export default {
   text-align: center
   opacity: 0.7
   background: white
-  color: #1D1D1D
+  color: black
   border-radius: 15px
-
-.animate-bounce
-  animation: q-bounce 2s infinite
-
-@keyframes q-bounce
-  0%, 20%, 50%, 80%, 100%
-    transform: translateY(0)
-  40%
-    transform: translateY(-30px)
-  60%
-    transform: translateY(-15px)
 
 
 </style>
