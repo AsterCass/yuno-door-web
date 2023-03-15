a64356f5ab47f6f7297e25b356ea2cf9efcc893a
astercasc <astercas@qq.com>
2023-03-15 16:19:22 +0800

    feat: 检索页card样式调整

--- src/components/CaskArticleList.vue
+++ src/components/CaskArticleList.vue
@@ -14,30 +14,15 @@
         <q-toggle color="dark" label="Windows" v-model="selection" val="Windows"/>
         <q-toggle color="dark" label="Java" v-model="selection" val="Java"/>
         <q-toggle color="dark" label="C/C++" v-model="selection" val="CCPlus"/>
-        <q-toggle color="dark" label="Yellow" v-model="selection" val="yellow"/>
+        <q-toggle color="dark" label="Mybatis" v-model="selection" val="Mybatis"/>
         <q-toggle color="dark" label="ML" v-model="selection" val="ML"/>
         <q-toggle color="dark" label="Sql" v-model="selection" val="Sql"/>
       </div>
     </div>
     <q-infinite-scroll @load="onLoad" :offset="250" class="col-12 row justify-center">
       <div v-for="(item, index) in items" :key="index" class="col-3">
-
-        <q-card class="admin-article-list-card bg-secondary text-white">
-          <q-card-section>
-            <div class="text-h6">Our Changing Planet</div>
-            <div class="text-subtitle2">by John Doe</div>
-          </q-card-section>
-          <q-card-section>
-            {{ item.key }}
-          </q-card-section>
-          <q-separator dark/>
-          <q-card-actions>
-            <q-btn flat>Action 1</q-btn>
-            <q-btn flat>Action 2</q-btn>
-          </q-card-actions>
-        </q-card>
+        <CaskArticleListCard :intro="item"/>
       </div>
-
       <template v-slot:loading>
         <div class="row justify-center" style="margin: 2rem">
           <q-spinner-cube color="dark" size="2rem"/>
@@ -51,6 +36,7 @@
 
 <script setup>
 import {ref} from "vue";
+import CaskArticleListCard from "@/components/CaskArticleListCard.vue";
 
 const lorem = ref('Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
     'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
@@ -93,8 +79,5 @@ function onLoad(index, done) {
 
 .admin-article-list-base
   padding: 1%
-
-.admin-article-list-card
-  margin: 5%
 
 </style>
---
+++ src/components/CaskArticleListCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-card class="admin-article-list-card">
+    <q-card-section>
+      <div class="admin-article-list-card-title">
+        <h6>CICD-FRONTEND</h6>
+      </div>
+    </q-card-section>
+    <q-card-section>
+      {{ intro.key }}
+    </q-card-section>
+    <q-separator spaced=".25rem" size="0.05rem"/>
+    <q-card-section>
+      <div class="text-subtitle2">by John Doe</div>
+      <div class="text-subtitle2">by John Doe</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+
+defineProps({
+  intro: {},
+});
+
+
+</script>
+
+<style lang="sass" scoped>
+@import "@/styles/cask.sass"
+
+.admin-article-list-card
+  color: $cask_dark_jungle_green
+  background-color: white
+  box-shadow: 5 10px 15px -3px rgba(0, 0, 0, .1), 0 4px 6px -2px rgba(0, 0, 0, .05)
+  border-radius: 1rem
+  font-size: 1rem
+  line-height: 1.625
+  margin: 5%
+
+.admin-article-list-card-title
+  padding: 1rem
+  text-align: left
+  transform: translateY(-15%)
+  margin: -1.5rem 50% 0rem  -2rem
+  background-image: linear-gradient(195deg, #42424a, #191919)
+  color: $cask_base_white
+  box-shadow: 0 4px 20px 0 rgba(0, 0, 0, .14), 0 7px 10px -5px rgba(64, 64, 64, .4)
+  border-radius: 1rem
+
+  h6
+    font-size: 1.4rem
+    font-weight: 600
+    color: white
+    margin: 0
+    font-family: Roboto Slab, sans-serif
+    text-transform: unset
+
+</style>
