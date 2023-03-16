05d2aa473ba4040b2f7f91127c32c1245466ae8b
astercasc <astercas@qq.com>
2023-03-16 13:40:03 +0800

    style: 代码结构调整

--- src/components/CaskAdminShow.vue
+++ src/components/CaskAdminShow.vue
@@ -42,71 +42,75 @@
         <q-icon name="fa-solid fa-trophy" size="1em"/>
         游戏分享
       </div>
-
-      <div class="col-10 row justify-start">
-        <div class="col-4 row admin-show-game-cell">
-          <q-btn @click="changeHex" :ripple="false" unelevated class="col-12 admin-show-game-btn">
-            <q-carousel
-                transition-prev="scale"
-                transition-next="scale"
-                transition-duration=1000
-                animated
-                infinite
-                v-model="hex"
-                height=100%
-                style="border-radius: 2rem"
-                class="admin-show-game-cell-carousel">
-              <q-carousel-slide class="admin-show-game-cell-inner" name="hex"
-                                :img-src="require('../assets/img/Hextris.png')"/>
-              <q-carousel-slide class="admin-show-game-cell-inner" name="hexDesc">
-                <div class="admin-show-game-cell-desc">
-                  <h6>
-                    {{ gameInfo.hexDesc.title }}
-                  </h6>
-                  <p v-for="count in gameInfo.hexDesc.desc" :key="count">
-                    {{ count }}
-                  </p>
-                  <q-btn label="Go Play"/>
-                </div>
-              </q-carousel-slide>
-            </q-carousel>
-
-          </q-btn>
-        </div>
-
-
-        <div class="col-4 row admin-show-game-cell">
-          <q-btn @click="changeMik" :ripple="false" unelevated class="col-12 admin-show-game-btn">
-            <q-carousel
-                transition-prev="scale"
-                transition-next="scale"
-                transition-duration=1000
-                animated
-                infinite
-                v-model="mik"
-                height=100%
-                style="border-radius: 2rem"
-                class="admin-show-game-cell-carousel">
-              <q-carousel-slide class="admin-show-game-cell-inner" name="mik"
-                                :img-src="require('../assets/img/Mikutap.png')"/>
-              <q-carousel-slide class="admin-show-game-cell-inner" name="mikDesc">
-                <div class="admin-show-game-cell-desc">
-                  <h6>
-                    {{ gameInfo.mikDesc.title }}
-                  </h6>
-                  <p v-for="count in gameInfo.mikDesc.desc" :key="count">
-                    {{ count }}
-                  </p>
-                  <q-btn label="Go Play"/>
-                </div>
-              </q-carousel-slide>
-            </q-carousel>
-
-          </q-btn>
-        </div>
-
-
+      <div class="col-10">
+        <CaskGameSimpleList/>
       </div>
+      <!--      <div class="col-10 row justify-start">-->
+      <!--        <div class="col-4 row admin-show-game-cell">-->
+      <!--          <q-btn @click="changeHex" :ripple="false" unelevated class="col-12 admin-show-game-btn">-->
+      <!--            <q-carousel-->
+      <!--                transition-prev="scale"-->
+      <!--                transition-next="scale"-->
+      <!--                transition-duration=1000-->
+      <!--                animated-->
+      <!--                infinite-->
+      <!--                v-model="hex"-->
+      <!--                height=100%-->
+      <!--                style="border-radius: 2rem"-->
+      <!--                class="admin-show-game-cell-carousel">-->
+      <!--              <q-carousel-slide class="admin-show-game-cell-inner" name="hex"-->
+      <!--                                :img-src="require('../assets/img/Hextris.png')"/>-->
+      <!--              <q-carousel-slide class="admin-show-game-cell-inner" name="hexDesc">-->
+      <!--                <div class="admin-show-game-cell-desc">-->
+      <!--                  <h6>-->
+      <!--                    {{ gameInfo.hexDesc.title }}-->
+      <!--                  </h6>-->
+      <!--                  <p v-for="count in gameInfo.hexDesc.desc" :key="count">-->
+      <!--                    {{ count }}-->
+      <!--                  </p>-->
+      <!--                  <q-btn label="Go Play"/>-->
+      <!--                </div>-->
+      <!--              </q-carousel-slide>-->
+      <!--            </q-carousel>-->
+
+      <!--          </q-btn>-->
+      <!--        </div>-->
+
+
+      <!--        <div class="col-4 row admin-show-game-cell">-->
+      <!--          <q-btn @click="changeMik" :ripple="false" unelevated class="col-12 admin-show-game-btn">-->
+      <!--            <q-carousel-->
+      <!--                transition-prev="scale"-->
+      <!--                transition-next="scale"-->
+      <!--                transition-duration=1000-->
+      <!--                animated-->
+      <!--                infinite-->
+      <!--                v-model="mik"-->
+      <!--                height=100%-->
+      <!--                style="border-radius: 2rem"-->
+      <!--                class="admin-show-game-cell-carousel">-->
+      <!--              <q-carousel-slide class="admin-show-game-cell-inner" name="mik"-->
+      <!--                                :img-src="require('../assets/img/Mikutap.png')"/>-->
+      <!--              <q-carousel-slide class="admin-show-game-cell-inner" name="mikDesc">-->
+      <!--                <div class="admin-show-game-cell-desc">-->
+      <!--                  <h6>-->
+      <!--                    {{ gameInfo.mikDesc.title }}-->
+      <!--                  </h6>-->
+      <!--                  <p v-for="count in gameInfo.mikDesc.desc" :key="count">-->
+      <!--                    {{ count }}-->
+      <!--                  </p>-->
+      <!--                  <q-btn label="Go Play"/>-->
+      <!--                </div>-->
+      <!--              </q-carousel-slide>-->
+      <!--            </q-carousel>-->
+
+      <!--          </q-btn>-->
+      <!--        </div>-->
+
+
+      <!--      </div>-->
+
+
     </div>
 
   </div>
@@ -114,37 +118,15 @@
 
 <script setup>
 
-import {ref, onMounted} from "vue";
+import {ref} from "vue";
 import CaskArticleListCard from "@/components/CaskArticleListCard.vue";
-import gameInfo from "@/constant/game-show-desc"
+import CaskGameSimpleList from "@/components/CaskGameSimpleList.vue";
 
 const lorem = ref('Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
     'sed do eiusmod tempor incididunt ut labore et dolore magna aliquafdasfdasfsdafda fasdfjdlsaf' +
     'lafjldasjflasdfjdsjf fljadslfjdslajfl fldsjafldsj fl lfjasdlfjdsal f fdlasjfldasj  fljdsaldfj flasjfdlj' +
     'fdasljfldsa lfjasdlf lafjl flsajf ldsajfl fjslafj lasjfl lfj lsa flajf la.')
 const items = ref([{key: lorem.value}, {key: lorem.value}, {key: lorem.value}])
-const hex = ref('hex')
-const mik = ref('mik')
-
-function changeHex() {
-  if (hex.value === 'hex') {
-    hex.value = 'hexDesc'
-  } else {
-    hex.value = 'hex'
-  }
-}
-
-function changeMik() {
-  if (mik.value === 'mik') {
-    mik.value = 'mikDesc'
-  } else {
-    mik.value = 'mik'
-  }
-}
-
-onMounted(() => {
-  console.log(gameInfo)
-})
 
 
 </script>
@@ -178,26 +160,6 @@ onMounted(() => {
 
 .admin-show-base-game-content
   height: 30rem
-
-.admin-show-game-cell
-  margin: 1rem
-  border-radius: 2rem
-
-  .admin-show-game-btn
-    padding: 0
-    border-radius: 2rem
-
-    .admin-show-game-cell-carousel
-      width: 100%
-
-      .admin-show-game-cell-inner
-        width: 100%
-        text-align: center
-
-        .admin-show-game-cell-desc
-          width: 100%
-          height: 100%
-          padding: 1rem
 
 
 </style>
---
+++ src/components/CaskGameSimpleList.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="row justify-start admin-show-game-out">
+    <div class="col-4 row admin-show-game-cell">
+      <q-btn @click="changeHex" :ripple="false" unelevated class="col-12 admin-show-game-btn">
+        <q-carousel
+            transition-prev="scale"
+            transition-next="scale"
+            transition-duration=1000
+            animated
+            infinite
+            v-model="hex"
+            height=100%
+            style="border-radius: 2rem"
+            class="admin-show-game-cell-carousel">
+          <q-carousel-slide class="admin-show-game-cell-inner" name="hex"
+                            :img-src="require('../assets/img/Hextris.png')"/>
+          <q-carousel-slide class="admin-show-game-cell-inner" name="hexDesc">
+            <div class="admin-show-game-cell-desc">
+              <h6>
+                {{ gameInfo.hexDesc.title }}
+              </h6>
+              <p v-for="count in gameInfo.hexDesc.desc" :key="count">
+                {{ count }}
+              </p>
+              <q-btn href="https://game.astercasc.com/G-Mikutap/" target="_blank" label="Go Play"/>
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+
+      </q-btn>
+    </div>
+
+
+    <div class="col-4 row admin-show-game-cell">
+      <q-btn @click="changeMik" :ripple="false" unelevated class="col-12 admin-show-game-btn">
+        <q-carousel
+            transition-prev="scale"
+            transition-next="scale"
+            transition-duration=1000
+            animated
+            infinite
+            v-model="mik"
+            height=100%
+            style="border-radius: 2rem"
+            class="admin-show-game-cell-carousel">
+          <q-carousel-slide class="admin-show-game-cell-inner" name="mik"
+                            :img-src="require('../assets/img/Mikutap.png')"/>
+          <q-carousel-slide class="admin-show-game-cell-inner" name="mikDesc">
+            <div class="admin-show-game-cell-desc">
+              <h6>
+                {{ gameInfo.mikDesc.title }}
+              </h6>
+              <p v-for="count in gameInfo.mikDesc.desc" :key="count">
+                {{ count }}
+              </p>
+              <q-btn href="https://game.astercasc.com/G-Hextris/" target="_blank" label="Go Play"/>
+            </div>
+          </q-carousel-slide>
+        </q-carousel>
+
+      </q-btn>
+    </div>
+
+
+  </div>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+import gameInfo from "@/constant/game-show-desc"
+
+const hex = ref('hex')
+const mik = ref('mik')
+
+function changeHex() {
+  if (hex.value === 'hex') {
+    hex.value = 'hexDesc'
+  } else {
+    hex.value = 'hex'
+  }
+}
+
+function changeMik() {
+  if (mik.value === 'mik') {
+    mik.value = 'mikDesc'
+  } else {
+    mik.value = 'mik'
+  }
+}
+
+</script>
+
+<style lang="sass" scoped>
+
+
+.admin-show-game-out
+  height: 100%
+
+  .admin-show-game-cell
+    margin: 1rem
+    border-radius: 2rem
+
+    .admin-show-game-btn
+      padding: 0
+      border-radius: 2rem
+
+      .admin-show-game-cell-carousel
+        width: 100%
+
+        .admin-show-game-cell-inner
+          width: 100%
+          text-align: center
+
+          .admin-show-game-cell-desc
+            width: 100%
+            height: 100%
+            padding: 1rem
+
+</style>
