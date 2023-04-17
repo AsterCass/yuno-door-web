013193391d61c9c38861173789c36989cad790d2
astercasc <astercas@qq.com>
2023-04-17 17:42:06 +0800

    feat: add not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    this is 404
+  <div style="text-align: center; padding-top: 15em;">
+    <h2>
+      404 Not Found
+    </h2>
+    <p style="font-size: 1.1rem">
+      页面地址错咯 回到主页重新看看吧
+    </p>
+    <br>
+    <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-house" label="主页" to="/"/>
   </div>
+
+  <div style="position: absolute; bottom: 0; right: 0;left: 0">
+    <CopyrightFooter/>
+  </div>
+
 </template>
 
 <script setup>
+import CopyrightFooter from "@/components/CopyrightFooter.vue";
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+.headerBtn
+  font-size: 1.5rem
+  padding-top: 0.8rem
+  font-weight: bold
 
 </style>
