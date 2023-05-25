35e920b9b123e544f1ec817018c1f3a90db38291
astercasc <astercas@qq.com>
2023-05-25 16:08:12 +0800

    feat: header标题位置相对全bar居中

--- src/components/CaskWebHeader.vue
+++ src/components/CaskWebHeader.vue
@@ -1,12 +1,19 @@
 <template>
   <q-header :modelValue="headerVisible" class="index-header">
-    <q-toolbar>
-      <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-house" label="home" to="/"/>
-      <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-book-bookmark" label="essay" to="/essay/list"/>
-      <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-book" label="article" to="/article/list"/>
-      <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-clapperboard" label="video" to="/video/collection"/>
-      <q-toolbar-title>Aster Casc</q-toolbar-title>
-      <LoginUserAvatar/>
+    <q-toolbar class="row justify-between">
+      <div class="col-4 row justify-start">
+        <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-house" label="home" to="/"/>
+        <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-book-bookmark" label="essay" to="/essay/list"/>
+        <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-book" label="article" to="/article/list"/>
+        <q-btn class="headerBtn" flat dense stack icon="fa-solid fa-clapperboard" label="video" to="/video/collection"/>
+      </div>
+      <div class="col-4 row justify-center">
+        <q-toolbar-title shrink>Aster Casc</q-toolbar-title>
+      </div>
+      <div class="col-4 row justify-end">
+        <LoginUserAvatar/>
+      </div>
+
     </q-toolbar>
   </q-header>
 </template>
